96a9fc955c3f225b659c58a635a2ef97f84c955f
joviclazar <ljvideos1@gmail.com>
2025-12-16 20:37:15 +0100

    Added section for informations in Profile View

---
+++ lavugio-front/src/app/features/view-profile/components/profile-info-section/profile-info-section.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProfileInfoRow } from '../profile-info-row/profile-info-row';
+
+@Component({
+  selector: 'app-profile-info-section',
+  imports: [ProfileInfoRow],
+  templateUrl: './profile-info-section.html',
+  styleUrl: './profile-info-section.css',
+})
+export class ProfileInfoSection {
+
+}
