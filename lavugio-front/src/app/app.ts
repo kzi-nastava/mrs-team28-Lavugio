919a36cc6c8b391005774ff92eedc640592af9a7
joviclazar <ljovic192@gmail.com>
2025-12-17 22:22:06 +0100

    Added NavBar

--- lavugio-front/src/app/app.ts
+++ lavugio-front/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+//import {BaseInfoPage} from './layout/base-info-page/base-info-page';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
--- lavugio-front/src/app/features/view-profile/profile-view/profile-view.ts
+++ lavugio-front/src/app/features/view-profile/profile-view/profile-view.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProfileHeader } from '../components/profile-header/profile-header';
 import { ProfileInfoSection } from '../components/profile-info-section/profile-info-section';
+import { Navbar } from "@app/layout/navbar/navbar";
 
 @Component({
   selector: 'app-profile-view',
-  imports: [ProfileHeader, ProfileInfoSection],
+  imports: [ProfileHeader, ProfileInfoSection, Navbar],
   templateUrl: './profile-view.html',
   styleUrl: './profile-view.css',
 })
