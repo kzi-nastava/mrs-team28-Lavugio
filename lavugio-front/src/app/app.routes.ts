23d7388b5d5dca09b31346273f3516342697aedb
Vukasin Vujovic <vujovic.sv11.2023@uns.ac.rs>
2025-12-18 02:26:16 +0100

    Add driver history page for table view and detailed view

--- lavugio-front/src/app/app.routes.ts
+++ lavugio-front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {TripHistoryDriver} from '@app/layout/trip-history-driver/trip-history-driver';
 import {BaseInfoPage} from '@app/layout/base-info-page/base-info-page';
+import { TripHistoryDriverDetailed } from './layout/trip-history-driver-detailed/trip-history-driver-detailed';
 
 export const routes: Routes = [
   {
@@ -11,4 +12,8 @@ export const routes: Routes = [
     path: '',
     component: BaseInfoPage,
   },
+  {
+    path:'trip-history-driver-detailed',
+    component: TripHistoryDriverDetailed,
+  }
 ];
--- lavugio-front/src/app/app.ts
+++ lavugio-front/src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {BaseInfoPage} from './layout/base-info-page/base-info-page';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BaseInfoPage],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
