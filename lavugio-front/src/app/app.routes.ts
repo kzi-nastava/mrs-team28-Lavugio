97b0ac8bf617f43d97ce3af6590017df623744ef
joviclazar <ljvideos1@gmail.com>
2025-12-18 10:02:21 +0100

    Added routint to Find Trip page

--- lavugio-front/src/app/app.routes.ts
+++ lavugio-front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 //import {TripHistoryDriver} from '@app/layout/trip-history-driver/trip-history-driver';
 //import {BaseInfoPage} from '@app/layout/base-info-page/base-info-page';
 import { ProfileView } from './features/view-profile/profile-view/profile-view';
+import { FindTrip } from './features/find-trip/find-trip';
 
 export const routes: Routes = [
   /*{
@@ -14,5 +15,8 @@ export const routes: Routes = [
   },*/
   {
         path: 'profile', title: "View Profile", component: ProfileView
+  },
+  {
+        path: 'find-trip', title: "Find Trip ", component: FindTrip
   }
 ];
