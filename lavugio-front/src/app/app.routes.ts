d9e768830c721e9dd12c4320689ecabfb9e7f470
Jovan Cvijanovic <jovan.cvijanovic2@gmail.com>
2025-12-18 01:10:02 +0100

    feat: ✨ Login(2.2.1) and Register(2.2.2) UI

--- lavugio-front/src/app/app.routes.ts
+++ lavugio-front/src/app/app.routes.ts
@@ -2,6 +2,12 @@ import { Routes } from '@angular/router';
 //import {TripHistoryDriver} from '@app/layout/trip-history-driver/trip-history-driver';
 //import {BaseInfoPage} from '@app/layout/base-info-page/base-info-page';
 import { ProfileView } from './features/view-profile/profile-view/profile-view';
+import { Login } from './features/login/login';
+import { ForgotPassword } from './features/forgot-password/forgot-password';
+import { ResetPassword } from './features/reset-password/reset-password';
+import { Register } from './features/register/register';
+import { VerifyEmail } from './features/verify-email/verify-email';
+import { VerificationSuccess } from './features/verification-success/verification-success';
 
 export const routes: Routes = [
   /*{
@@ -14,5 +20,23 @@ export const routes: Routes = [
   },*/
   {
         path: 'profile', title: "View Profile", component: ProfileView
+  },
+  {
+        path: 'login', title: 'Login', component: Login   
+  },
+  {
+        path: 'register', title: 'Register', component: Register
+  },
+  {
+        path: 'verify-email', title: 'Verify Email', component: VerifyEmail
+  },
+  {
+        path: 'verification-success', title: 'Verification Success', component: VerificationSuccess
+  },
+  {
+        path: 'forgot-password', title: 'Forgot Password', component: ForgotPassword
+  },
+  {
+        path: 'reset-password', title: 'Reset Password', component: ResetPassword
   }
 ];
