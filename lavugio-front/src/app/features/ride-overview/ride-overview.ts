bcf486076629609820822033819688a97c2e3604
Vukasin Vujovic <vujovic.sv11.2023@uns.ac.rs>
2026-01-21 00:53:41 +0100

    Add path parameter to ride overview

--- lavugio-front/src/app/features/ride-overview/ride-overview.ts
+++ lavugio-front/src/app/features/ride-overview/ride-overview.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ViewChild, effect, Injector, inject, EffectRef, signal } from '@angular/core';
-import { Router } from '@angular/router';
+import { AfterViewInit, Component, ViewChild, effect, Injector, inject, signal } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Navbar } from '@app/shared/components/navbar/navbar';
 import { MapComponent } from '@app/shared/components/map/map';
 import { RideInfo } from './ride-info/ride-info';
@@ -23,23 +23,21 @@ export class RideOverview implements AfterViewInit {
   private injector = inject(Injector);
   private rideService = inject(RideService);
   private driverService = inject(DriverService);
-  
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
   isInfoOpen = signal(false);
   isDesktop = signal(window.innerWidth >= 1024);
   showReport = signal(false);
   showReview = signal(false);
   isRated = signal(false);
   isReported = signal(false);
-  
+
   rideOverview = signal<RideOverviewModel | null>(null);
-  rideId: number = 1;
-  
+  rideId!: number;
+
   private intervalId: any;
   private subscription: Subscription | null = null;
-  private router = inject(Router);
-  navigateToCancelRide() {
-    this.router.navigate([`/cancel-ride/${this.rideId}`]);
-  }
 
   @ViewChild('rideInfo') rideInfo!: RideInfo;
   @ViewChild('map') mapComponent!: MapComponent;
@@ -51,6 +49,14 @@ export class RideOverview implements AfterViewInit {
   }
 
   ngOnInit(): void {
+    const rideIdParam = this.route.snapshot.paramMap.get('rideId');
+    if (rideIdParam) {
+      this.rideId = +rideIdParam;
+    } else {
+      console.error("RideId missing from url");
+      return;
+    }
+
     this.fetchRideOverview(this.rideId);
     this.createTopicSubscription(this.rideId);
   }
@@ -60,7 +66,6 @@ export class RideOverview implements AfterViewInit {
       const overview = this.rideOverview();
       if (overview && overview.checkpoints && overview.checkpoints.length > 0) {
         const points = overview.checkpoints;
-        console.log("Points:", points);
         this.mapComponent?.setRoute(points);
         this.mapComponent?.addMarker(points[0], MarkerIcons.start);
         this.mapComponent?.addMarker(points[points.length - 1], MarkerIcons.end);
@@ -84,40 +89,25 @@ export class RideOverview implements AfterViewInit {
         return EMPTY;
       })
     ).subscribe(overview => {
-      console.log('Ride Overview fetched:', overview);
       this.rideOverview.set(overview);
       this.isReported.set(overview.reported ? true : false);
       this.isRated.set(overview.reviewed ? true : false);
-      console.log(overview.reported)
-      console.log(overview.reviewed)
     });
   }
 
-  toggleInfo() {
-    this.isInfoOpen.update(v => !v);
-  }
-
-  closeInfo() {
-    this.isInfoOpen.set(false);
-  }
-
-  openInfo() {
-    this.isInfoOpen.set(true);
-  }
+  toggleInfo() { this.isInfoOpen.update(v => !v); }
+  closeInfo() { this.isInfoOpen.set(false); }
+  openInfo() { this.isInfoOpen.set(true); }
 
   executeInterval(driverId: number): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
+    if (this.intervalId) clearInterval(this.intervalId);
 
     this.driverService.getDriverLocation(driverId).subscribe((location: { location: Coordinates }) => {
       let mark = this.addDriverLocationMarker(location.location);
-      
+
       this.intervalId = setInterval(() => {
         this.driverService.getDriverLocation(driverId).subscribe((newLocation: { location: Coordinates }) => {
-          if (mark) {
-            this.mapComponent?.removeMarker(mark);
-          }
+          if (mark) this.mapComponent?.removeMarker(mark);
           mark = this.addDriverLocationMarker(newLocation.location);
         });
       }, 60000);
@@ -128,16 +118,12 @@ export class RideOverview implements AfterViewInit {
     let mark: Marker | undefined;
     if (driverLoc) {
       mark = this.mapComponent?.addMarker(driverLoc, MarkerIcons.driverAvailable);
-      console.log('Added driver location marker at:', driverLoc);
-    } else {
-      console.warn('Driver location is not available to add marker.');
     }
     return mark;
   }
 
   createTopicSubscription(rideId: number): void {
     this.rideService.listenToRideUpdates(rideId).subscribe(update => {
-      console.log('Received ride update via WebSocket:', update);
       const currentOverview = this.rideOverview();
       if (currentOverview) {
         const updatedOverview = this.applyRideOverviewUpdate(currentOverview, update);
@@ -157,7 +143,7 @@ export class RideOverview implements AfterViewInit {
       checkpoints: updatedCheckpoints,
       status: update.status !== undefined ? update.status : current.status,
       price: update.price !== undefined ? update.price : current.price,
-      departureTime: update.departureTime !== undefined 
+      departureTime: update.departureTime !== undefined
         ? (update.departureTime ? new Date(update.departureTime) : null)
         : current.departureTime,
       arrivalTime: update.arrivalTime !== undefined
@@ -166,9 +152,9 @@ export class RideOverview implements AfterViewInit {
     };
   }
 
-  cancelRide(){
+  cancelRide() {
     const current = this.rideOverview();
-    if (current != null){
+    if (current != null) {
       this.rideOverview.set({
         ...current,
         status: "CANCELLED"
@@ -176,6 +162,9 @@ export class RideOverview implements AfterViewInit {
     }
   }
 
+  navigateToCancelRide() {
+    this.router.navigate([`/cancel-ride/${this.rideId}`]);
+  }
 
   ngOnDestroy(): void {
     this.subscription?.unsubscribe();
