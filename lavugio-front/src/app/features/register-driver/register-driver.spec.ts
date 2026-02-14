4b0a1784fc7553106bee5006a32510f4da7bf286
joviclazar <ljvideos1@gmail.com>
2026-02-14 22:26:01 +0100

    Added frontend service test

--- lavugio-front/src/app/features/register-driver/register-driver.spec.ts
+++ lavugio-front/src/app/features/register-driver/register-driver.spec.ts
@@ -1,23 +1,111 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
+import { provideRouter } from '@angular/router';
 
 import { RegisterDriver } from './register-driver';
+import { DriverService } from '@app/core/services/user/driver-service';
+import { DialogService } from '@app/core/services/dialog-service';
+import { Router } from '@angular/router';
 
 describe('RegisterDriver', () => {
   let component: RegisterDriver;
   let fixture: ComponentFixture<RegisterDriver>;
 
+  let driverServiceMock: any;
+  let dialogServiceMock: any;
+  let router: Router;
+
   beforeEach(async () => {
+    driverServiceMock = {
+      registerDriver: jasmine.createSpy().and.returnValue(of({})),
+    };
+
+    dialogServiceMock = {
+      open: jasmine.createSpy(),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [RegisterDriver]
-    })
-    .compileComponents();
+      imports: [RegisterDriver],
+      providers: [
+        provideRouter([]),
+        { provide: DriverService, useValue: driverServiceMock },
+        { provide: DialogService, useValue: dialogServiceMock },
+      ],
+    }).compileComponents();
+
+    router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
 
     fixture = TestBed.createComponent(RegisterDriver);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call service and open success dialog', () => {
+    component.currentStep = 1;
+
+    component.driverForm = {
+      onSubmit: () => {},
+      isFormValid: () => true,
+    } as any;
+
+    component.vehicleForm = {
+      onSubmit: () => {},
+      isFormValid: () => true,
+    } as any;
+
+    component.driverData = {
+      email: 'a@a.com',
+      password: '123',
+      name: 'Marko',
+      surname: 'Markovic',
+      phoneNumber: '123',
+      address: 'Adresa',
+    };
+
+    component.vehicleData = {
+      make: 'BMW',
+      model: 'X5',
+      licensePlate: 'NS-1234-AA',
+      color: 'Black',
+      vehicleType: 'Standard',
+      seats: 4,
+      petFriendly: false,
+      babyFriendly: false,
+    };
+
+    component.finishRegistration();
+
+    expect(driverServiceMock.registerDriver).toHaveBeenCalled();
+    expect(dialogServiceMock.open).toHaveBeenCalledWith(
+      'Registration Successful',
+      jasmine.any(String),
+      false,
+    );
+  });
+
+  it('should open error dialog on service error', () => {
+    driverServiceMock.registerDriver.and.returnValue(
+      throwError(() => ({
+        error: { message: 'Error occurred' },
+      })),
+    );
+
+    component.currentStep = 1;
+
+    component.driverForm = { onSubmit: () => {}, isFormValid: () => true } as any;
+    component.vehicleForm = { onSubmit: () => {}, isFormValid: () => true } as any;
+
+    component.finishRegistration();
+
+    expect(dialogServiceMock.open).toHaveBeenCalledWith(
+      'Registration Failed',
+      'Error occurred',
+      true,
+    );
   });
 });
--- lavugio-front/src/app/features/register-driver/vehicle-form/vehicle-form.spec.ts
+++ lavugio-front/src/app/features/register-driver/vehicle-form/vehicle-form.spec.ts
@@ -9,15 +9,44 @@ describe('VehicleForm', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [VehicleForm]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(VehicleForm);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be invalid when empty', () => {
+    expect(component.isFormValid()).toBeFalse();
   });
+
+  it('should validate correct license plate', () => {
+    component.make.set('BMW');
+    component.model.set('X5');
+    component.licensePlate.set('NS-1234-AA');
+    component.seats.set(4);
+    component.color.set('Black');
+
+    expect(component.isFormValid()).toBeTrue();
+  });
+
+  it('should be invalid with incorrect license plate', () => {
+    component.make.set('BMW');
+    component.model.set('X5');
+    component.seats.set(4);
+    component.color.set('Black');
+    
+    component.licensePlate.set('NS-1234');
+    expect(component.isFormValid()).toBeFalse();
+
+    component.licensePlate.set('1234-NS-AA');
+    expect(component.isFormValid()).toBeFalse();
+
+    component.licensePlate.set('NS-1234-AAA');
+    expect(component.isFormValid()).toBeFalse();
+
+    component.licensePlate.set('Ns-123-AA');
+    expect(component.isFormValid()).toBeFalse();
+  });
+
 });
